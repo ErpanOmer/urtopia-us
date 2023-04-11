5526957b2c814081a086cd1c69c97c68479f89bd
ErpanOmer <354073823@qq.com>
2023-04-11 15:00:30 +0800

    feature: 新增邮件弹窗

---
+++ assets/shopInfoList-us-3.js
@@ -0,0 +1,168 @@
+var shopInfoList = {
+    Fremont: {
+        "510-795-9622": {
+            name: "Bicycle Garage Fremont",
+            testRideSize: "M",
+            testrideSpot: "Fremont",
+            imgUrl: "https://cdn.shopify.com/s/files/1/0583/5810/4213/files/testrideshopimg-Bicycle_Garage_Fremont_fremont.jpg?v=1665374978",
+            add: "5006 Mowry Avenue, Fremont, CA 94538",
+            timezone: "Fremont, California, USA (GMT-7)",
+            email: "bikeshoptina@gmail.com",
+            phone: "510-795-9622",
+            businessHours: ["11:00\u201317:00", "10:00\u201319:00", "", "10:00\u201319:00", "10:00\u201319:00", "10:00\u201319:00", "10:00\u201318:00"]
+        }
+    },
+    "Newport Beach": {
+        "949-274-7944": {
+            name: "Groove E-Bikes ",
+            testRideSize: "M",
+            testrideSpot: "Newport Beach",
+            imgUrl: "https://cdn.shopify.com/s/files/1/0583/5810/4213/files/test-ride-groove_ebike.png?v=1670312992",
+            add: "3740 Campus Drive, Suite A, Newport Beach, 92660",
+            timezone: "Newport Beach, CA, USA (GMT-8)",
+            email: "bob@grooveebikes.com ",
+            phone: "949-274-7944",
+            businessHours: ["11:00\u201315:00", "11:00\u201318:00", "11:00\u201318:00", "11:00\u201318:00", "11:00\u201318:00", "11:00\u201318:00", "11:00\u201318:00"]
+        }
+    },
+    "Los Angeles": {
+        "310-862-5001": {
+            name: "Bike Attack Playa Vista",
+            testRideSize: "L",
+            testrideSpot: "Los Angeles",
+            imgUrl: "https://cdn.shopify.com/s/files/1/0583/5810/4213/files/testrideshopimg-BikeAttackPlayaVista.jpg?v=1665374977",
+            add: "12775 W Millennium, Los Angeles, CA 90094",
+            timezone: "Los Angeles, CA, USA (GMT-7)",
+            email: "sales@bikeattack.com",
+            phone: "310-862-5001",
+            businessHours: ["10:00\u201318:00", "10:00\u201318:00", "10:00\u201318:00", "10:00\u201318:00", "10:00\u201318:00", "10:00\u201318:00", "10:00\u201318:00"]
+        },
+        "213-533-8000": {
+            name: "DTLA Bikes",
+            testRideSize: "M",
+            testrideSpot: "Los Angeles",
+            imgUrl: "https://cdn.shopify.com/s/files/1/0583/5810/4213/files/testrideshopimg-dtla_bikes.jpg?v=1665374977",
+            add: "425 S. Broadway, Los Angeles, CA 90013",
+            timezone: "Los Angeles, CA, USA (GMT-7)",
+            email: "rodney@dtlabikes.com",
+            phone: "213-533-8000",
+            businessHours: ["09:30\u201318:30", "09:30\u201318:30", "09:30\u201318:30", "09:30\u201318:30", "09:30\u201318:30", "09:30\u201318:30", ""]
+        }
+    },
+    "Silicon Valley": {
+        "888-612-9883": {
+            name: "ELV Motors",
+            testRideSize: "M/L",
+            testrideSpot: "Silicon Valley",
+            imgUrl: "https://cdn.shopify.com/s/files/1/0583/5810/4213/files/testrideshopimg-ELV_Motors_ca_san_jose.jpg?v=1665374977",
+            add: "2332 El Camino Real, Santa Clara, CA 95050",
+            timezone: "Santa Clara, CA, USA (GMT-7)",
+            email: "doug@elvmotors.com",
+            phone: "888-612-9883",
+            businessHours: ["12:00\u201316:00", "11:00\u201318:00", "11:00\u201318:00", "11:00\u201318:00", "11:00\u201318:00", "11:00\u201318:00", "11:00\u201317:00"]
+        }
+    },
+    "San Diego": {
+        "619-786-7161": {
+            name: "Eze Ryders Electric Bikes",
+            testRideSize: "M",
+            testrideSpot: "San Diego",
+            imgUrl: "https://cdn.shopify.com/s/files/1/0583/5810/4213/files/testrideshopimg-Eze_Ryders_Electric_Bikes_san_dieago.jpg?v=1665374977",
+            add: "4051 Voltaire St Ste C, San Diego, CA 92107",
+            timezone: "San Diego, CA, USA (GMT-7)",
+            email: "manager@ezeryders.com, sales@ezeryders.com",
+            phone: "619-786-7161",
+            businessHours: ["10:00\u201317:00", "", "12:00\u201317:00", "12:00\u201317:00", "12:00\u201317:00", "12:00\u201317:00", "10:00\u201317:00"]
+        }
+    },
+    "New York": {
+        "718-433-0450": {
+            name: "Spokesman Cycles (LIC)",
+            testRideSize: "L",
+            testrideSpot: "New York",
+            imgUrl: "https://cdn.shopify.com/s/files/1/0583/5810/4213/files/testrideshopimg-Spokesman_Cycles_LIC_ny.jpg?v=1665374977",
+            add: "49-04 Vernon Blvd, Long Island City, NY 11101",
+            timezone: "Long Island City, Queens, NY, USA (GMT-4)",
+            email: "bikesnyc@gmail.com",
+            phone: "718-433-0450",
+            businessHours: ["10:00\u201319:00", "10:00\u201319:00", "10:00\u201319:00", "10:00\u201319:00", "10:00\u201319:00", "10:00\u201319:00", "10:00\u201319:00"]
+        },
+        "929-295-0955": {
+            name: "Spokesman Cycles (Glendale)",
+            testRideSize: "M",
+            testrideSpot: "New York",
+            imgUrl: "https://cdn.shopify.com/s/files/1/0583/5810/4213/files/testrideshopimg-Spokesman_Cycles_Midtown.jpg",
+            add: "78-17 Myrtle Ave, Queens, NY 11385",
+            timezone: "Queens, NY, USA (GMT-4)",
+            email: "bikesnyc@gmail.com",
+            phone: "929-295-0955",
+            businessHours: ["", "", "11:00\u201319:00", "11:00\u201319:00", "11:00\u201319:00", "11:00\u201319:00", "11:00\u201319:00"]
+        }
+    },
+    "Washington DC": {
+        "202-209-8563": {
+            name: "Buna Bicycle Works",
+            testRideSize: "M",
+            testrideSpot: "Washington DC",
+            imgUrl: "https://cdn.shopify.com/s/files/1/0583/5810/4213/files/testrideshopimg-Buna_Bicycle_Works_Washington_dc.jpg?v=1665374977",
+            add: "4828 MacArthur Blvd NW, Washington, DC 20007",
+            timezone: "Washington, DC, USA (GMT-4)",
+            email: "guy@bunabicycleworks.com",
+            phone: "202-209-8563",
+            businessHours: ["08:00\u201315:00", "08:00\u201318:00", "08:00\u201318:00", "08:00\u201318:00", "08:00\u201318:00", "08:00\u201318:00", "08:00\u201315:00"]
+        }
+    },
+    Orlando: {
+        "407-614-4539": {
+            name: "Wheel Works E-Bikes",
+            testRideSize: "M",
+            testrideSpot: "Orlando",
+            imgUrl: "https://cdn.shopify.com/s/files/1/0583/5810/4213/files/testrideshopimg-Wheel_Works_E-Bikes.jpg?v=1667368641",
+            add: "855 E Plant St Suite 600, Winter Garden, FL 34787",
+            timezone: "Orlando, FL (GMT-4)",
+            email: "ebikes@wgwheelworks.com",
+            phone: "407-614-4539",
+            businessHours: ["", "10:00\u201318:00", "10:00\u201318:00", "10:00\u201318:00", "10:00\u201318:00", "10:00\u201318:00", "09:00\u201317:00"]
+        }
+    },
+    Tampa: {
+        "+1 352-600-6422": {
+            name: "E-Cycle Adventures",
+            testRideSize: "M",
+            testrideSpot: "Tampa",
+            imgUrl: "https://cdn.shopify.com/s/files/1/0583/5810/4213/files/testrideshopimg-E-Cycle_Adventures_fl.jpg?v=1665374977",
+            add: "9904 Little Rd, New Port Richey, FL 34654",
+            timezone: "New Port Richey, FL, USA (GMT-4)",
+            email: "ecycleadventures@gmail.com",
+            phone: "+1 352-600-6422",
+            businessHours: ["", "10:00\u201318:00", "10:00\u201318:00", "10:00\u201318:00", "10:00\u201318:00", "10:00\u201318:00", "10:00\u201318:00"]
+        }
+    },
+    Dallas: {
+        "214-370-9010": {
+            name: "Playtri",
+            testRideSize: "M",
+            testrideSpot: "Dallas",
+            imgUrl: "https://cdn.shopify.com/s/files/1/0583/5810/4213/files/testrideshopimg-Playtri_dallas.jpg?v=1665374977",
+            add: "6465 E Mockingbird Ln #358, Dallas, TX 75214",
+            timezone: "Dallas, TX, USA (GMT-5)",
+            email: "store@playtri.com",
+            phone: "214-370-9010",
+            businessHours: ["12:00\u201317:00", "11:00\u201319:00", "11:00\u201319:00", "11:00\u201319:00", "11:00\u201319:00", "11:00\u201319:00", "09:00\u201317:00"]
+        }
+    },
+    Sarasota: {
+        "941-730-5565 ": {
+            name: "Playtri Sarasota",
+            testRideSize: "M",
+            testrideSpot: "Sarasota",
+            imgUrl: "https://cdn.shopify.com/s/files/1/0583/5810/4213/files/testrideshopimg-Playtri_Sarasota.jpg?v=1668230046",
+            add: "315 N Cattlemen Road Unit 5, Sarasota, FL 34235",
+            timezone: "Sarasota, FL (GMT-5)",
+            email: "sarasota@playtri.com",
+            phone: "941-730-5565 ",
+            businessHours: ["12:00\u201317:00", "11:00\u201319:00", "11:00\u201319:00", "11:00\u201319:00", "11:00\u201319:00", "11:00\u201319:00", "09:00\u201317:00"]
+        }
+    }
+};
+//# sourceMappingURL=/s/files/1/0583/5810/4213/files/shopInfoList-us-3.js.map?v=1674893114
