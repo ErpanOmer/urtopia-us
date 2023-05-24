8f986e77bef3cfab50ae21a904a3d1a0cfc2b45c
ErpanOmer <354073823@qq.com>
2023-05-24 10:55:22 +0800

    merge: 合并代码

---
+++ assets/global_config.js
@@ -0,0 +1,34 @@
+// 发货时间映射表
+// key:    vairant_id
+// value:  发货时间文案
+const ebike_delivery_time = {
+    // carbon 1
+    43556899913976: 'Ships within <span>3 business days</span> from L.A. warehouse.',
+    43556899979512: 'Ships within <span>3 business days</span> from L.A. warehouse.',
+    43556899946744: 'Ships within <span>3 business days</span> from L.A. warehouse.',
+    43645495443704: 'Ships within <span>3 business days</span> from L.A. warehouse.',
+    43556900012280: 'Ships within <span>3 business days</span> from L.A. warehouse.',
+    43556900077816: 'Ships within <span>3 business days</span> from L.A. warehouse.',
+    43556900045048: 'Ships within <span>3 business days</span> from L.A. warehouse.',
+    43645495476472: 'Ships within <span>3 business days</span> from L.A. warehouse.',
+    // carbon 1s
+    43556899651832: 'Ships within <span>3 business days</span> from L.A. warehouse.',
+    43556896014584: 'Ships within <span>3 business days</span> from L.A. warehouse.',
+    43556899684600: 'Ships within <span>3 business days</span> from L.A. warehouse.',
+    43556899750136: 'Ships within <span>3 business days</span> from L.A. warehouse.',
+    43556899881208: 'Ships within <span>3 business days</span> from L.A. warehouse.',
+
+    43556895916280: 'Shipped from <span>June 1-15, 2023 </span>from L.A. warehouse.',
+    43556896047352: 'Shipped from <span>June 1-15, 2023 </span>from L.A. warehouse.',
+
+    43556899619064: 'Shipped from <span>June 15-30, 2023 </span>from L.A. warehouse.',
+    43645495345400: 'Shipped from <span>June 15-30, 2023 </span>from L.A. warehouse.',
+    43556899815672: 'Shipped from <span>June 15-30, 2023 </span>from L.A. warehouse.',
+    43556895981816: 'Shipped from <span>June 15-30, 2023 </span>from L.A. warehouse.',
+    43645495378168: 'Shipped from <span>June 15-30, 2023 </span>from L.A. warehouse.',
+    43556895949048: 'Shipped from <span>June 15-30, 2023 </span>from L.A. warehouse.',
+    43556896080120: 'Shipped from <span>June 15-30, 2023 </span>from L.A. warehouse.',
+    43645495410936: 'Shipped from <span>June 15-30, 2023 </span>from L.A. warehouse.',
+    // chord
+    // chord x
+}
