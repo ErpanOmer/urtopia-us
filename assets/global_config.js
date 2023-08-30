366512f76d304a065a1d2708b3dcd60124120d9a
354073823@qq.com <354073823@qq.com>
2023-08-30 18:02:42 +0800

    merge: 美国活动开发

--- assets/global_config.js
+++ assets/global_config.js
@@ -130,7 +130,7 @@ const global_config = {
             //lb l
             43556896014584: 'Ships between <span>September 1-15, 2023 </span>from L.A. warehouse.',
             //pb s
-            43556899684600: 'Ships between <span>October 1-15, 2023 </span>from L.A. warehouse.',
+            43556899684600: 'Ships between <span>September 15-30, 2023 </span>from L.A. warehouse.',
             //pw s
             43556899750136: 'Ships within <span>5 business days</span> from L.A. warehouse.',
             //pw l
