3c9c513de8f68d301629f57008a536ee7a8230c1
354073823@qq.com <354073823@qq.com>
2023-09-15 18:52:13 +0800

    feature: 活动开发

--- assets/global_config.js
+++ assets/global_config.js
@@ -160,6 +160,8 @@ const global_config = {
     chord_order_page_config: {
         // chord 车默认显示 variant_id, 只有 url 上的 variant 参数为空的时候生效
         default_variant: 43694976663800,
+        // variant 为 available:false 的情况下 也要强制显示 
+        ignore_variants_available_false: [],
         // 产品图
         product_images: {
             "High-Step": {
