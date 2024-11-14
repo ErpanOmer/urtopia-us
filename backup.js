ac52bcd7b5bdab69dc7678826c7259976fa893f6
ErpanOmer <354073823@qq.com>
2024-11-14 18:03:13 +0800

    merge: 代码合并

--- backup.js
+++ backup.js
@@ -33,6 +33,6 @@ function run (command) {
 
 await run(`git checkout master`)
 await run(`git checkout -B ${version}`)
-// await run(`npm run pull`)
+await run(`npm run pull`)
 await run(`git push origin ${version} -f`)
 await run(`git checkout master`)
