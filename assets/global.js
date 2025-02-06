78bf55e7fa7deab8af86a38dda87fe1ebb0a7f70
ErpanOmer <354073823@qq.com>
2025-02-06 13:34:57 +0800

    merge: 代码合并

--- assets/global.js
+++ assets/global.js
@@ -2326,3 +2326,22 @@ function lozad (selector = '.lozad', options = {}) {
   lozad('.lozad', {
     rootMargin: '1000px 2000px' // syntax similar to that of CSS Margin
   }).observe('.lozad');
+
+  fetch(window.location.pathname + '?sections=footer').then(r => r.json()).then(r => {
+    task(() => {
+      $(document.body).append($(r['footer']))
+    })
+
+    task(() => {
+      $(".mobile-footer .footer-block__heading").click(function () {
+        $(this).siblings("ul.footer-block__details-content").slideToggle()
+        .parent().siblings().find("ul.footer-block__details-content").slideUp()
+      })
+    })
+  })
+
+  fetch(window.location.pathname + '?sections=cart-drawer').then(r => r.json()).then(r => {
+    task(() => {
+      $(document.body).append($(r['cart-drawer']))
+    })
+  })
