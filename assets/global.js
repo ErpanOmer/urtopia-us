9574d26ac9f01c54704776c1e4333b3e93bf1139
ErpanOmer <354073823@qq.com>
2025-04-01 10:02:50 +0800

    merge: 代码合并

--- assets/global.js
+++ assets/global.js
@@ -2231,7 +2231,7 @@ window.addEventListener('load', async () => {
   }
 
   
-  if (![/\/pages\/carbon-expert/, /\/pages\/lightweight-ebike-urtopia-carbon-1-pro/, /\/collections\/urtopia-black-friday-sale/, /\/pages\/email-page/].some(pattern => pattern.test(window.location.pathname))) {
+  if (![/\/collections\/e-bike-sale/, /\/collections\/e-bikes/, /\/pages\/carbon-expert/, /\/pages\/lightweight-ebike-urtopia-carbon-1-pro/, /\/collections\/urtopia-black-friday-sale/, /\/pages\/email-page/].some(pattern => pattern.test(window.location.pathname))) {
     task(() => {
       showSubscribeEmailDialog().then(showTestRideDialog)
     }, 5000)
