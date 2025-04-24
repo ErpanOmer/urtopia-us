d51ea7af9d3e040c1810b0b39b9f3ed5a0a54ec0
ErpanOmer <354073823@qq.com>
2025-04-24 17:45:55 +0800

    merge: 代码合并

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ export default {
         },
       },
       fontFamily: {
-        base: ['Helvetica Now Display', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Ubuntu', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif', "Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"],
+        base: ['Inter', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Ubuntu', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif', "Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"],
       },
       colors: {
         primary: '#FD4B17',
